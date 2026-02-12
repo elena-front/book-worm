8c7ee80746ca5e0b49108432391948e4e6fa0496
Niks <ktm.softwear@gmail.com>
2026-02-12 21:12:01 +0300

    DB_damn

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '127.0.0.1'
-  }
-})
+    host: '127.0.0.1',
+    proxy: {
+      '/books': 'http://localhost:3000',
+      '/reviews': 'http://localhost:3000',
+      '/favorites': 'http://localhost:3000',
+      '/auth': 'http://localhost:3000',
+    },
+  },
+});
