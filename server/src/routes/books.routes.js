08d8ccd6973bb808d738685e754184bdb9488e33
Niks <ktm.softwear@gmail.com>
2026-02-12 14:47:48 +0300

    server 1.2

--- server/src/routes/books.routes.js
+++ server/src/routes/books.routes.js
@@ -6,11 +6,13 @@ const {
   getAllBooks,
   getBookById,
   createBook,
+  getBookReviews,
 } = require("../controllers/books.controller");
 
 // GET /books
 router.get("/", getAllBooks);
-router.get("/:id", getBookById);
 router.post("/", createBook);
+router.get("/:id/reviews", getBookReviews);
+router.get("/:id", getBookById);
 
 module.exports = router;
