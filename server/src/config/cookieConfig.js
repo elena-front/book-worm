7f3ec07cd7f320de95ed7016b5940d35ccc2c08a
elena-front <shumkova.helen@yandex.ru>
2026-06-19 00:16:33 +0400

    Fix CORS

--- server/src/config/cookieConfig.js
+++ server/src/config/cookieConfig.js
@@ -1,4 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7,
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
 };
